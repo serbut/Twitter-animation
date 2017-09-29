75d3d2ffbce44fd3e2172504587e3e8d13a57401
serbut <serbut555@gmail.com>
2017-09-29 12:46:58 +0300

    Initial Commit

---
+++ Twitter Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Animation
+//
+//  Created by Сергей Буторин on 29/09/2017.
+//  Copyright © 2017 Sergey Butorin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

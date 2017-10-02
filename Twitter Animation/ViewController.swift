91e362a16a478b4adfd2aea7c17c79e9d407bdf3
serbut <serbut555@gmail.com>
2017-10-02 18:37:57 +0300

    Task completed

--- Twitter Animation/ViewController.swift
+++ Twitter Animation/ViewController.swift
@@ -20,8 +20,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var categoryButtonsView: UIView!
     @IBOutlet weak var headerImageOverlay: UIView!
     
-    let headerMinOffset: CGFloat = 50
-    let offsetInfoHeader: CGFloat = 85.0
+    let headerMinOffset: CGFloat = 50.0
+    let offsetInfoHeader: CGFloat = 94.0
     let positionInfoHeader: CGFloat = 50.0
     let maxAlphaFactor: CGFloat = 0.8
     
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        updateCategoryButtons(scrollView.contentOffset.y)
+        categoryButtonsView.layer.transform = calculateCategoryButtonsTransform(withOffset: scrollView.contentOffset.y)
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,23 +58,28 @@ extension ViewController: UIScrollViewDelegate {
         
         let offset = scrollView.contentOffset.y
         
-        updateHeader(offset)
-        updateCategoryButtons(offset)
-        if offset > 0 {
-            updateAvatar(offset)
-            updateHeaderPosition(offset)
+        var headerTransform = CATransform3DIdentity
+        var avatarTransform = CATransform3DIdentity
+        
+        headerTransform = calculateHeaderTransform(withOffset: offset)
+        if offset >= 0 {
+            avatarTransform = calculateAvatarTransform(withOffset: offset)
+            updateHeaderPosition(withOffset: offset)
         }
+        
+        headerView.layer.transform = headerTransform
+        avatarImageView.layer.transform = avatarTransform
+        categoryButtonsView.layer.transform = calculateCategoryButtonsTransform(withOffset: offset)
     }
     
-    func updateHeader(_ offset: CGFloat) {
+    func calculateHeaderTransform(withOffset offset: CGFloat) -> CATransform3D {
         var headerTransform = CATransform3DIdentity
         
         if offset < 0 {
             let headerScaleFactor:CGFloat = -(offset) / headerView.bounds.height
-            let headerSizevariation = ((headerView.bounds.height * (1.0 + headerScaleFactor)) - headerView.bounds.height)/2.0
-            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+            let headerSizeVariation = ((headerView.bounds.height * (1.0 + headerScaleFactor)) - headerView.bounds.height)/2.0
+            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizeVariation, 0)
             headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
-            headerView.layer.transform = headerTransform
         } else {
             headerTransform = CATransform3DTranslate(headerTransform, 0, max(-headerMinOffset, -offset), 0)
             let headerInfoTransform = CATransform3DMakeTranslation(0, max(-positionInfoHeader, offsetInfoHeader - offset), 0)
@@ -82,28 +87,29 @@ extension ViewController: UIScrollViewDelegate {
         }
         
         headerImageOverlay.alpha = min(maxAlphaFactor, abs(offset/50))
-        headerView.layer.transform = headerTransform
+        return headerTransform
     }
     
-    func updateCategoryButtons(_ offset: CGFloat) {
+    func calculateCategoryButtonsTransform(withOffset offset: CGFloat) -> CATransform3D {
         let categoryButtonsOffset = profileView.frame.height - categoryButtonsView.frame.height - offset
         var categoryButtonsTransform = CATransform3DIdentity
         categoryButtonsTransform = CATransform3DTranslate(categoryButtonsTransform, 0, max(categoryButtonsOffset, -headerMinOffset), 0)
-        categoryButtonsView.layer.transform = categoryButtonsTransform
+        
+        return categoryButtonsTransform
     }
     
-    func updateAvatar(_ offset: CGFloat) {
+    func calculateAvatarTransform(withOffset offset: CGFloat) -> CATransform3D {
         var avatarTransform = CATransform3DIdentity
 
-        let avatarScaleFactor = (min(headerMinOffset, offset)) / avatarImageView.bounds.height / 1.8
+        let avatarScaleFactor = (min(headerMinOffset, offset)) / avatarImageView.bounds.height / 2.2 // изменяем размер аватара
         let avatarSizeVariation = ((avatarImageView.bounds.height * (1.0 + avatarScaleFactor)) - avatarImageView.bounds.height) / 2.0
         avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
         avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
         
-        avatarImageView.layer.transform = avatarTransform
+        return avatarTransform
     }
     
-    func updateHeaderPosition(_ offset: CGFloat) {
+    func updateHeaderPosition(withOffset offset: CGFloat) {
         if offset <= headerMinOffset {
             if avatarImageView.layer.zPosition < headerView.layer.zPosition{
                 headerView.layer.zPosition = 0
